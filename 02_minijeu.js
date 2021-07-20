8812171515cea9ff90411f6991f7d662569238d1
dbutelle <butelledylan@gmail.com>
2021-07-20 10:41:02 +0200

    Add commentary

--- 02_minijeu.js
+++ 02_minijeu.js
@@ -4,14 +4,22 @@
  Si bonne réponse -> console.log('bravo'+numéro mystere)
  Sinon 'pas de chance' + nouveau jeu + numéro mystere
  */
+/**
+ * Function which generate a number between min and max
+ * @param min
+ * @param max
+ * @returns {number}
+ */
 function createRandom(min,max){
     let answer = Math.floor(Math.random()*(max - min) + min);
     return answer;
 }
-
+//Variable declaration
 let nbTry=1,win=false,answer = createRandom(1,10),human = [1,2,3],nbTryMax = 4;
 
-
+/**
+ * Function which verify if the game is finish
+ */
 function play(){
     if(answer<human[nbTry-1]){
         console.log("Plus petit")
@@ -22,10 +30,12 @@ function play(){
         console.log(`Bravo ! Vous avez trouvé le ${answer} en ${nbTry} essais`);
     }
 }
+//While it's not finish
 while(nbTryMax<4 && !win){
     play();
     nbTryMax+=1;
 }
+//If it's finish
 if(nbTryMax==4){
     console.log(`Le numéro à trouver était le ${answer}`);
 }
